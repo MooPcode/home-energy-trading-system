cb83c6a582dddd794247564fec3886d3183609a8
Mitchell Williamson <mitchell.williamson97@gmail.com>
2017-10-08 20:40:11 +1100

    Add basic home and appliance communication

--- src/appliances/ApplianceAgent.java
+++ src/appliances/ApplianceAgent.java
@@ -1,15 +1,50 @@
 package appliances;
 
 import jade.core.Agent;
+import java.util.Iterator;
+import jade.core.AID;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
 
 public class ApplianceAgent extends Agent {
 
 	public ApplianceAgent() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	protected void setup() {
+		// Send messages to two agents whose names are "a1" and "a2" (hard-coded)
+		ACLMessage msg = new ACLMessage(ACLMessage.SUBSCRIBE);
+		msg.addReceiver(new AID("home", AID.ISLOCALNAME));
+		send(msg);
+		
+		addBehaviour(new CyclicBehaviour(this) {
+			public void action() {
+				//System.out.println(getLocalName() + ": Waiting for message");
+				ACLMessage msg = receive();
+				if (msg != null) {
+					// Handle message
+					System.out.println(getLocalName()+ ": Received message " + msg.getContent() + " from " + msg.getSender().getLocalName());
+					
+					// Get usage command
+					if(msg.getContent().contains("GetUsage"))
+					{
+						// Reply to message
+						ACLMessage reply = msg.createReply();
+						reply.setPerformative(ACLMessage.INFORM);
+						reply.setContent(String.valueOf(getUsage()));
+						// Send reply
+						System.out.println(getLocalName() + ": Sending response " +
+						reply.getContent() + " to " + msg.getAllReceiver().next());
+						send(reply);	
+					}
+			}
+			}
+		});
+	}
 
-	public int GetUsage()
+	public int getUsage()
 	{
-		return 5;
+		return (int) Math.round(Math.random() * 10);
 	}
 }
--- src/home/HomeAgent.java
+++ src/home/HomeAgent.java
@@ -1,17 +1,85 @@
 package home;
 
 import java.util.*;
+
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.lang.acl.*;
 import appliances.*;
 import retailers.*;
 
 public class HomeAgent extends Agent {
 	RetailerAgent retailer;
 	List<RetailerAgent> retailers;
-	List<ApplianceAgent> appliances;
+	List<AID> appliances = new ArrayList<AID>();
+	Map<AID, Integer> applianceUsage = new HashMap<AID, Integer>();
 	
 	public HomeAgent() {
 		// changing this for a test commit
+	}
+	
+	protected void setup() {
+		// First set-up message receiving behaviour
+		CyclicBehaviour messageListeningBehaviour = new CyclicBehaviour(this) {
+			public void action() {
+				ACLMessage msg = receive();
+				while(msg != null) {
+					if(msg.getPerformative() == ACLMessage.INFORM)
+					{
+						System.out.println(getLocalName() + ": Received response " + msg.getContent() + " from " + msg.getSender().getLocalName());
+						applianceUsage.put(msg.getSender(), Integer.valueOf(msg.getContent()));
+					} else if(msg.getPerformative() == ACLMessage.SUBSCRIBE)
+					{
+						appliances.add(msg.getSender());
+						applianceUsage.put(msg.getSender(), null);
+						System.out.println(msg.getSender().getLocalName() + " subscribed to home");
+					}
+					
+					msg = receive();
+				}
+				block();
+			}
+		};
+		addBehaviour(messageListeningBehaviour);
+		
+		CyclicBehaviour getUsage = new CyclicBehaviour(this) {
+			@Override
+			public void action() {
+				ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+				msg.setContent("GetUsage");
+				
+				for(AID appliance : appliances)
+				{
+					msg.addReceiver(appliance);
+				}
+				
+				send(msg);
+			}
+		};
+		addBehaviour(getUsage);
+		
+		/**
+		// Send messages to two agents whose names are "a1" and "a2" (hard-coded)
+		ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+		msg.setContent("GetUsage");
+		msg.addReceiver(new AID("appliance", AID.ISLOCALNAME));
+		// Send Message (only once)
+		System.out.println(getLocalName() + ": Sending message " + msg.getContent() + " to ");
+		Iterator receivers = msg.getAllIntendedReceiver();
+		while (receivers.hasNext()) {
+			System.out.println(((AID) receivers.next()).getLocalName());
+		}
+		send(msg);**/
+	}
+	
+	public void doThings()
+	{
+		// ask appliances for the things
+		// appliances send back the things
+		
+		// ask retailers for the things
+		// retails send back the things
 	}
 	
 	public void tick()
